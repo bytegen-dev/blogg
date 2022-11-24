3fa75be1d768cbab8ca2692f33b62e0b70c079be
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-24 09:48:02 +0100

    finally

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ function drop(){
     // alert ('hey')
     ham.classList.toggle('showburger')
     ash.classList.toggle('show-link')
+
+    if (ham.className == 'hamburger showburger') {
+        setTimeout(()=>{
+            ham.classList.remove('showburger')
+            ash.classList.remove('show-link')
+        }, 10000)
+    }
 }
