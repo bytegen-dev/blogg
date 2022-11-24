0e71365d68f6293a35a9293c53a9da2e00d91d35
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-24 03:04:10 +0100

    final touches

---
+++ script.js
@@ -0,0 +1,8 @@
+var ham = document.querySelector('.hamburger')
+var ash = document.querySelector('.link-holder')
+
+function drop(){
+    // alert ('hey')
+    ham.classList.toggle('showburger')
+    ash.classList.toggle('show-link')
+}
